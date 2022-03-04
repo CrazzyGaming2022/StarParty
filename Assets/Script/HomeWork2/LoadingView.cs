bde40ba30e7fb168b9bddf61ddc607b2c9d51292
lipindim <lipindim@gmail.com>
2022-03-04 22:34:19 +1000

    Домашняя работа 4

---
+++ Assets/Script/HomeWork2/LoadingView.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingView : MonoBehaviour
+{
+    [SerializeField] private Image _loadingImage;
+    [SerializeField] private float _speed;
+
+    private bool _direct;
+
+    private void Update()
+    {
+        if (_direct)
+        {
+            _loadingImage.fillAmount += _speed;
+            if (_loadingImage.fillAmount >= 1)
+                _direct = false;
+        }
+        else
+        {
+            _loadingImage.fillAmount -= _speed;
+            if (_loadingImage.fillAmount <= 0)
+                _direct = true;
+        }
+    }
+
+    public void Show()
+    {
+        _loadingImage.gameObject.SetActive(true);
+        _direct = true;
+        _loadingImage.fillAmount = 0;
+    }
+
+    public void Hide()
+    {
+        _loadingImage.gameObject.SetActive(false);
+    }
+}
+
